446ca7de8577fa36ff5ee6318974b2f59eb2d363
jjackkun <jjackkun@gmail.com>
2018-09-27 18:53:43 +0900

    걸프 기본 세팅 작업중
    - 라우터에서 ejs를 활용한 템플릿 불러오기

--- gulp/serverSet.js
+++ gulp/serverSet.js
@@ -7,7 +7,10 @@ import PATH from 'Dir' ;
 const server = () => {	
 	console.log( '\n\n[ server ]' ) ; 
 	return new Promise( ( resolve , reject ) => {
-		gulp.src( `${ PATH.appRoot }/${ PATH.SRC.SERVER }/**/*.js` )
+		gulp.src([
+			`${ PATH.appRoot }/${ PATH.SRC.SERVER }/**/*.js` , 
+			`!${ PATH.appRoot }/${ PATH.SRC.SERVER }/{template,template/**}` 
+			])
 			.pipe( babel({
 				"presets" : ['es2015', 'es2017', 'stage-3' , 'react'],
 				"plugins" : [
@@ -29,6 +32,15 @@ const server = () => {
 	}) ;
 } ; 
 
+const template = () => {
+	return new Promise( ( resolve , reject ) => {
+		console.log( '\n\n[ template ]' ) ; 
+		gulp.src( `${ PATH.appRoot }/${ PATH.SRC.SERVER }/template/**` )
+			.pipe( gulp.dest( `${ PATH.appRoot }/${ PATH.DEST.SERVER }/template`) )
+			.on( 'finish' , resolve ) ; 
+	}) ;
+} ; 
+
 const nodemonSet = () => {	
 	return new Promise( ( resolve , reject ) => {
 		console.log( '\n\n[ nodemonSet ]' ) ; 
@@ -42,6 +54,6 @@ const nodemonSet = () => {
 	}) ;
 } ;
 
-const serverSet = gulp.series( server , nodemonSet ) ; 
+const serverSet = gulp.series( server , template , nodemonSet ) ; 
 
 export default serverSet ;  
--- workspace/src/server/app.js
+++ workspace/src/server/app.js
@@ -1,3 +1,7 @@
+import ejs from 'ejs' ; 
+import fs from 'fs' ; 
+import React from 'react' ; 
+import ReactDOMServer from 'react-dom/server';
 import express from 'express' ; 
 import PATH from './Dir' ; 
 
@@ -7,10 +11,14 @@ app.set( 'view engine' , 'ejs' ) ;
 app.engine( 'html' , require( 'ejs' ).renderFile ) ;  
 app.use( '/' , express.static( `/${ PATH.appRoot }/build` )) ; 
 
-
 app.get( '/' , ( req , res ) => {
-	res.end( 'yaho' ) ; 
+
+	let template = ejs.compile(fs.readFileSync(__dirname + '/template/default.ejs', 'utf8'));
+	res.send(template({
+		title : 'yaho' , 
+	})) ; 
 }) ; 
+
 
 const server = app.listen( PATH.PORT , () => {
 	console.log( 'Express listening on port : ' +  server.address().port ) ;
